34dc0d22aba6c3cb5e0ae139129dcd2580f5af54
JonnyKong <jonnykong1996@gmail.com>
2022-05-22 16:10:22 -0400

    Bug fixes

--- lua/semantics.lua
+++ lua/semantics.lua
@@ -26,6 +26,11 @@ inoremap <silent><expr> <TAB>
       \ coc#refresh()
 inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
 
+function! CheckBackspace() abort
+  let col = col('.') - 1
+  return !col || getline('.')[col - 1]  =~# '\s'
+endfunction
+
 nmap <silent> gd <Plug>(coc-definition)
 nmap <silent> gy <Plug>(coc-type-definition)
 nmap <silent> gi <Plug>(coc-implementation)
