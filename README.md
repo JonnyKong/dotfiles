9cfe2a65ee8f1585e9eacae11460df906de6af84
JonnyKong <jonnykong1996@gmail.com>
2022-08-20 16:54:57 -0400

    Use dotbot for automation

--- README.md
+++ README.md
@@ -2,13 +2,6 @@
 
 ### Setup
 
-Install [packer.nvim](https://github.com/wbthomason/packer.nvim)
-
-```
-ln -s <this_repo>/.config/nvim/init.lua init.lua
-ln -s <this_repo>/.config/nvim/lua lua
-ln -s <this_repo>/.config/nvim/coc-settings.json coc-settings.json
-
-:PackerSync
-:CocCommand clangd.install
-```
+```bash
+git clone https://github.com/JonnyKong/dotfiles.git --recurse-submodules --remote-submodules
+./install
--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -3,11 +3,17 @@ vim.g.material_theme_style = 'default'
 vim.g.airline_theme = 'material'
 vim.g.material_terminal_italics = 1
 
+require('material').setup({
+	contrast = {
+		sidebars = true, -- Enable contrast for sidebar-like windows ( for example Nvim-Tree )
+		floating_windows = true, -- Enable contrast for floating windows
+	},
+})
+
+vim.g.material_style = "darker"
 vim.cmd([[
-colorscheme material
+" colorscheme material
 " colorscheme github_dark_default
 " colorscheme PaperColor
-" colorscheme terafox
+colorscheme terafox
 ]])
-
-vim.g.material_style = "oceanic"
