e6d9e0055b81f7f147379ec53028fcb5817472a1
JonnyKong <jonnykong1996@gmail.com>
2022-10-29 20:03:38 +0000

    Changing filename color based on modified status

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -186,6 +186,29 @@ require('lspconfig')['clangd'].setup{ on_attach = on_attach }
 require('lspconfig')['jdtls'].setup{ on_attach = on_attach }
 require('lspconfig')['bashls'].setup{ on_attach = on_attach }
 
+local custom_fname = require('lualine.components.filename'):extend()
+local highlight = require'lualine.highlight'
+local default_status_colors = { saved = '#228B22', modified = '#C70039' }
+
+function custom_fname:init(options)
+  custom_fname.super.init(self, options)
+  self.status_colors = {
+    saved = highlight.create_component_highlight_group(
+      {bg = default_status_colors.saved}, 'filename_status_saved', self.options),
+    modified = highlight.create_component_highlight_group(
+      {bg = default_status_colors.modified}, 'filename_status_modified', self.options),
+  }
+  if self.options.color == nil then self.options.color = '' end
+end
+
+function custom_fname:update_status()
+  local data = custom_fname.super.update_status(self)
+  data = highlight.component_format_highlight(vim.bo.modified
+                                              and self.status_colors.modified
+                                              or self.status_colors.saved) .. data
+  return data
+end
+
 require('lualine').setup {
   options = {
     icons_enabled = true,
@@ -207,8 +230,8 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_b = {'branch', 'diagnostics'},
+    lualine_c = {custom_fname},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
