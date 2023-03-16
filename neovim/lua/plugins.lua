cebd58d1d46d5ed68ff19f293a21ce378e7676d3
JonnyKong <jonnykong1996@gmail.com>
2023-03-16 19:53:01 +0000

    Make indentline highlight current scope

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -261,3 +261,11 @@ require('lualine').setup {
   inactive_winbar = {},
   extensions = {}
 }
+
+require("indent_blankline").setup {
+    -- for example, context is off by default, use this to turn it on
+    show_current_context = true,
+    show_current_context_start = false,
+    -- treesitter to calculate indentation when possible
+    use_treesitter=true,
+}
