b4043924680b0e05439295493cb31ea1aaf111ab
JonnyKong <jonnykong1996@gmail.com>
2023-03-27 21:35:06 -0400

    Show change status with bold+italic

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -208,29 +208,6 @@ require('lspconfig')['pyright'].setup{ on_attach = on_attach }
 require('lspconfig')['clangd'].setup{ on_attach = on_attach }
 require('lspconfig')['bashls'].setup{ on_attach = on_attach }
 
-local custom_fname = require('lualine.components.filename'):extend()
-local highlight = require'lualine.highlight'
-local default_status_colors = { saved = '#228B22', modified = '#C70039' }
-
-function custom_fname:init(options)
-  custom_fname.super.init(self, options)
-  self.status_colors = {
-    saved = highlight.create_component_highlight_group(
-      {bg = default_status_colors.saved}, 'filename_status_saved', self.options),
-    modified = highlight.create_component_highlight_group(
-      {bg = default_status_colors.modified}, 'filename_status_modified', self.options),
-  }
-  if self.options.color == nil then self.options.color = '' end
-end
-
-function custom_fname:update_status()
-  local data = custom_fname.super.update_status(self)
-  data = highlight.component_format_highlight(vim.bo.modified
-                                              and self.status_colors.modified
-                                              or self.status_colors.saved) .. data
-  return data
-end
-
 require('lualine').setup {
   options = {
     icons_enabled = true,
@@ -252,7 +229,12 @@ require('lualine').setup {
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {custom_fname},
+    lualine_b = {{
+      'filename',
+      color = function(section)
+        return { gui = vim.bo.modified and 'italic,bold' or '' }
+      end,
+    }},
     lualine_c = {{ navic.get_location, cond = navic.is_available }},
     lualine_x = {'searchcount', 'fileformat', 'filetype'},
     lualine_y = {'location'},
