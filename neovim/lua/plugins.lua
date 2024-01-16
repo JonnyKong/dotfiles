12603fa19e6a581d564b1cfa8fae0fdd68aa53a7
JonnyKong <jonnykong1996@gmail.com>
2024-01-16 01:54:39 +0000

    Disable treesitter on large files

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -79,14 +79,21 @@ cmd "autocmd FileType c,cpp,java setlocal commentstring=//\\ %s"
 
 require("nvim-autopairs").setup {}
 
-require'nvim-treesitter.configs'.setup {
+require("nvim-treesitter.configs").setup {
   -- A list of parser names, or "all"
   ensure_installed = { "c", "cpp", "java", "python", "bash", "lua" },
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
   highlight = {
     enable = true,    
-  }
+  },
+  disable = function(lang, buf)
+    local max_filesize = 100 * 1024 -- 100 KB
+    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+    if ok and stats and stats.size > max_filesize then
+      return true
+    end
+  end,
 }
 
 require('gitsigns').setup()
