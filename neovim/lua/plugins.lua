776d470b2563f7e4379c55e583494447ee453160
JonnyKong <jonnykong1996@gmail.com>
2025-03-27 05:06:23 +0000

    Force OSC52 clipboard provider

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -284,3 +284,5 @@ vim.keymap.set('n', '<S-Left>', require('smart-splits').resize_left)
 vim.keymap.set('n', '<S-Down>', require('smart-splits').resize_down)
 vim.keymap.set('n', '<S-Up>', require('smart-splits').resize_up)
 vim.keymap.set('n', '<S-Right>', require('smart-splits').resize_right)
+
+vim.g.clipboard = 'osc52'
